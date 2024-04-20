0263bd3e2a58bbd7ecfe940c75e3f6e745e31c7d
Daniyal Anjum <d.anjum786@gmail.com>
2024-04-20 17:50:04 +0500

    search by category add

--- controllers/productController.js
+++ controllers/productController.js
@@ -89,18 +89,23 @@ const deleteProductController = async (req, res) => {
 
 const allProductsController = async (req, res) => {
   try {
-    const searchQuery = req.query.q; // Assuming the search term is passed as a query parameter named 'q'
-    let products;
+    const searchQuery = req.query.q; // Search query parameter
+    const categoryFilter = req.query.category; // Category filter parameter
 
+    let query = {}; // Initialize an empty query object
+
+    // If a search query is provided, add it to the query object
     if (searchQuery) {
-      // If a search term is provided, search for products whose name contains the search term
-      products = await productModel.find({
-        name: { $regex: searchQuery, $options: "i" },
-      });
-    } else {
-      // If no search term is provided, retrieve all products
-      products = await productModel.find({});
+      query.name = { $regex: searchQuery, $options: "i" }; // Case-insensitive search by name
     }
+
+    // If a category filter is provided, add it to the query object
+    if (categoryFilter) {
+      query.category = categoryFilter;
+    }
+
+    // Use the query object to find products that match the search query and/or category filter
+    const products = await productModel.find(query);
 
     if (!products || products.length === 0) {
       return res
